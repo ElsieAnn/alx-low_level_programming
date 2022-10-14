5d1906be9eae85679a2c54dc163858071d0cdaa7
ElsieAnn <elsieannquarm4@gmail.com>
2022-10-14 15:01:17 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Return:By default returns zero if no errors otherwise returns a non-zero value.
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
