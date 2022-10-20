06c724ce1eafd3a310443ba0c1708530c829ab60
ElsieAnn <elsieannquarm4@gmail.com>
2022-10-19 17:57:10 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ * @x: print int x
+ * @y: print int y
+ * Return: 0 (success)
+ */
+
+ int add ( int x , int y )
+
+ {
+ 	return (x + y)
+ }	        	
